4f7f96923b279a1c686ca8836a7f6c4cbf5f9977
lasha tsulukidze <121624660+LashaTsulukidze751@users.noreply.github.com>
2023-10-01 12:48:47 +0400

    Add files via upload

--- myPortfolio/script.js
+++ myPortfolio/script.js
@@ -20,31 +20,55 @@ languagesPercentages.forEach(element => {
     elementIndex++;
 })
 //day/night theme changer
-let icon =document.querySelector(".dayNightChanger");
-icon.addEventListener("click",()=>{
+let icon = document.querySelector(".dayNightChanger");
+icon.addEventListener("click", () => {
     document.body.classList.toggle("dark");
-    if(document.body.classList.contains("dark")){
-        icon.src="images/night-mode.png";
-    }else{
-        icon.src="images/brightness.png";
+    if (document.body.classList.contains("dark")) {
+        icon.src = "images/night-mode.png";
+    } else {
+        icon.src = "images/brightness.png";
     }
-    
+
 })
 //message sender
 let nameInput = document.getElementById(`name`);
 let emailInput = document.getElementById(`email`);
 let messageInput = document.getElementById(`message`);
-document.getElementById("messageSendBtn").onclick = function(){
-    if(nameInput.value != "" && emailInput.value != "" && messageInput.value != ""){
-        document.querySelector(".sentMessage").innerHTML="message sent"
-    } else{
-        document.querySelector(".sentMessage").innerHTML="message not sent,  please fill emtpy space "
+document.getElementById("messageSendBtn").onclick = function () {
+    if (nameInput.value != "" && emailInput.value != "" && messageInput.value != "") {
+        document.querySelector(".sentMessage").innerHTML = "message sent";
+        document.getElementById("messageSendBtn").addEventListener("mouseover", () => {
+            this.classList.add("sent");
+            this.addEventListener("mouseout", () => {
+                this.classList.remove("sent");
+            })
+        })
+    } else {
+        document.querySelector(".sentMessage").innerHTML = "message not sent,  please fill empty space. ";
+        document.getElementById("messageSendBtn").addEventListener("mouseover", () => {
+            this.classList.add("notsent");
+            this.addEventListener("mouseout", () => {
+                this.classList.remove("notsent");
+            })
+        })
     }
 };
 //contact me dencer
-document.getElementById("addres").onmouseover = ( )=>{
-    document.querySelector(".location").classList.add("danceup");
-    setTimeout( () => {
-        document.querySelector(".location").classList.remove("danceup")
-    },200);  
+document.getElementById("addres").onmouseover = () => {
+    document.querySelector(".addres-svg").classList.add("addresdance");
+    setTimeout(() => {
+        document.querySelector(".addres-svg").classList.remove("addresdance")
+    }, 200);
+}
+document.getElementById("phone").onmouseover = () => {
+    document.querySelector(".phone-svg").classList.add("addresdance");
+    setTimeout(() => {
+        document.querySelector(".phone-svg").classList.remove("addresdance")
+    }, 200);
+}
+document.getElementById("mail").onmouseover = () => {
+    document.querySelector(".mail-svg").classList.add("addresdance");
+    setTimeout(() => {
+        document.querySelector(".mail-svg").classList.remove("addresdance ")
+    }, 200);
 }
