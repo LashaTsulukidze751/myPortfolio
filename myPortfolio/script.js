89afa41be16dd3e386af261970b62619c72bc6e0
lasha tsulukidze <121624660+LashaTsulukidze751@users.noreply.github.com>
2023-10-03 20:45:42 +0400

    Add files via upload

--- myPortfolio/script.js
+++ myPortfolio/script.js
@@ -1,74 +1,12 @@
-
-document.querySelector(".spining-smile").addEventListener("mouseover", () => {
-    document.querySelector(".spining-smile").classList.add("spinner");
-    setTimeout(() => {
-        document.querySelector(".spining-smile").classList.add("spinner-remover");
-        setTimeout(() => {
-            document.querySelector(".spining-smile").classList.remove("spinner");
-            document.querySelector(".spining-smile").classList.remove("spinner-remover");
-        }, 1000)
-    }, 3000)
-});
-//languages percentages
-const languagesPercentages = document.querySelectorAll(".percentage-text");
-let elementIndex = 0;
-languagesPercentages.forEach(element => {
-    document.querySelectorAll(".percentage-text")[elementIndex].innerHTML = (`${element.textContent}%`)
-    percentage = parseFloat(element.textContent);
-    percentage = percentage / 100 * 200;
-    document.querySelectorAll(".inner")[elementIndex].style.width = `${percentage}px`;
-    elementIndex++;
+document.querySelector(".dropdownBtn").addEventListener("click",()=>{
+    document.querySelector(".dropdown").classList.toggle("show");
 })
-//day/night theme changer
-let icon = document.querySelector(".dayNightChanger");
-icon.addEventListener("click", () => {
-    document.body.classList.toggle("dark");
-    if (document.body.classList.contains("dark")) {
-        icon.src = "images/night-mode.png";
-    } else {
-        icon.src = "images/brightness.png";
-    }
-
+const inners = document.querySelectorAll(".inner");
+const percentages = document.querySelectorAll(".percentage");
+inners.forEach((inner)=>{
+    inner.style.width= inner.dataset.width;
 })
-//message sender
-let nameInput = document.getElementById(`name`);
-let emailInput = document.getElementById(`email`);
-let messageInput = document.getElementById(`message`);
-document.getElementById("messageSendBtn").onclick = function () {
-    if (nameInput.value != "" && emailInput.value != "" && messageInput.value != "") {
-        document.querySelector(".sentMessage").innerHTML = "message sent";
-        document.getElementById("messageSendBtn").addEventListener("mouseover", () => {
-            this.classList.add("sent");
-            this.addEventListener("mouseout", () => {
-                this.classList.remove("sent");
-            })
-        })
-    } else {
-        document.querySelector(".sentMessage").innerHTML = "message not sent,  please fill empty space. ";
-        document.getElementById("messageSendBtn").addEventListener("mouseover", () => {
-            this.classList.add("notsent");
-            this.addEventListener("mouseout", () => {
-                this.classList.remove("notsent");
-            })
-        })
-    }
-};
-//contact me dencer
-document.getElementById("addres").onmouseover = () => {
-    document.querySelector(".addres-svg").classList.add("addresdance");
-    setTimeout(() => {
-        document.querySelector(".addres-svg").classList.remove("addresdance")
-    }, 200);
-}
-document.getElementById("phone").onmouseover = () => {
-    document.querySelector(".phone-svg").classList.add("addresdance");
-    setTimeout(() => {
-        document.querySelector(".phone-svg").classList.remove("addresdance")
-    }, 200);
-}
-document.getElementById("mail").onmouseover = () => {
-    document.querySelector(".mail-svg").classList.add("addresdance");
-    setTimeout(() => {
-        document.querySelector(".mail-svg").classList.remove("addresdance ")
-    }, 200);
+
+for(let i=0;i<percentages.length;i++){
+    percentages[i].innerHTML=inners[i].dataset.width;
 }
